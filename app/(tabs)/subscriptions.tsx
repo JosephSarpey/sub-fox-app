18bcc20f84a4a9f357afa537e8c78bd74a37cf55
JosephSarpey <josephsarpey2002@gmail.com>
2026-09-02 06:01:14 +0530

    feat: project setup, nativewind, routing, tab navigations

--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 const Subscriptions = () => {
   return (
-    <View>
+    <View className="flex-1 bg-background p-5">
       <Text>Subscriptions</Text>
     </View>
   );
